3fa0178c115b54905708ae0c951eada398053533
noshenko98 <noshenko98@gmail.com>
2026-05-19 17:32:53 +0300

    Create model Manufacturer

--- equipment_information/models.py
+++ equipment_information/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name} ({self.country})"
