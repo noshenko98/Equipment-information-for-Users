0cc8ce6f502569bd76296d5d2a80aa478935fb47
noshenko98 <noshenko98@gmail.com>
2026-06-19 14:07:02 +0300

    Chenge settings and requirements for deployment

--- equipment_information_for_users/settings.py
+++ equipment_information_for_users/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,13 +20,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-3d*r6z*_d0l&t0j28_=3$yo&)5f(jx!v&_#-4h2d30qi&!73o4"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "l8dwa@)gPlLq2!hSDQ+HwIH-&X!4Qd^gVdF}V{M<3Y9>;k3haWo&4")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
+if not DEBUG:
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_SSL_REDIRECT = True
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
 
 # Application definition
 
